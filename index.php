37ae770b1348918c1e592e8abfc38e80efdb931e
oraclerevolution <mac.n.drix@gmail.com>
2019-07-17 10:51:08 +0000

    version 1.1.1 - ajout de connexion

--- index.php
+++ index.php
@@ -1,3 +1,60 @@
+<?php
+session_start();
+require './scripts/database.php';
+$username = $motDePasse = $usernameError = $motDePasseError = "";
+
+function inputVerify($var){
+
+    $var = trim($var);
+    $var = htmlspecialchars($var);
+    $var = stripslashes($var);
+
+    return $var;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = inputVerify($_POST['username']);
+    $motDePasse = inputVerify($_POST['password']);
+
+    if (empty($username)) {
+        $usernameError = "Vous n'avez pas entré de nom d'utilisateur";
+    }
+
+    if (empty($motDePasse)) {
+        $motDePasseError = "Vérifiez que vous avez saisi un mot de passe";
+    }
+
+    if(!empty($_POST['username']) and !empty($_POST['password']) )
+    {
+        $db = Database::connect();
+        $req=$db->prepare("SELECT * from users where username=? AND  password=?");
+        $req->execute(array($username,$motDePasse));
+
+
+        $userHasBeenFound = $req->rowCount();
+        if ($userHasBeenFound) {
+            $user = $req->fetch(PDO::FETCH_OBJ);
+            $_SESSION['id'] = $user->id;
+            $id=$_SESSION['id'];
+
+            $_SESSION['username'] = $user->username;
+            $username = $_SESSION['username'];
+
+            $_SESSION['isAdmin'] = $user->isAdmin;
+            $isadmin = $_SESSION['isAdmin'];
+
+            $_SESSION['NIV0'] = 1 ;
+        }
+        if ($userHasBeenFound) {
+            header('Location: ./accueil.php');
+
+
+        }
+    }
+
+}
+?>
 <!DOCTYPE html>
 <html>
 <?php include('scripts/head.php'); ?>
@@ -11,14 +68,16 @@
   <div class="login-box-body">
     <p class="login-box-msg">Connectez-vous pour demarrer votre session</p>
 
-    <form action="../../index2.html" method="post">
+    <form action="index.php" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="id agent">
+        <input type="text" class="form-control" name="username" placeholder="id agent">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <p style="color: red"> <?php echo $usernameError; ?></p>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="mot de passe">
+        <input type="password" class="form-control" name="password" placeholder="mot de passe">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        <p style="color: red"> <?php echo $motDePasseError; ?></p>
       </div>
       <div class="row">
         <div class="col-xs-4">
